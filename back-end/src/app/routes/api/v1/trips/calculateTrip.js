3614ba84b8e4beef5b6c928f204d7aae71b16810
Luke Soguero <lukesoguero@gmail.com>
2020-04-26 21:36:54 -0600

    fixed duplicate waypoint bug, fixed bug where one extra waypoint was returned, improved error messages

--- back-end/src/app/routes/api/v1/trips/calculateTrip.js
+++ back-end/src/app/routes/api/v1/trips/calculateTrip.js
@@ -18,10 +18,11 @@ module.exports = Router({mergeParams: true})
       const errors = validationResult(req);
       if (!errors.isEmpty()) {return res.status(422).json({ errors: errors.array() }) }
       try {
-          var route = await calculateTrip(req.query);
-          res.status(200).json(route);
+        var route = await calculateTrip(req.query);
+        res.status(200).json(route);
       } catch(error) {
-          next(error)
+        req.logger.info({error: error});
+        res.status(500).send(error);
       }
 });
 
@@ -37,7 +38,13 @@ async function calculateTrip(params) {
     var types = params.types == "" ? [] : params.types.split(',');
     types.push("tourist_attraction");
     // if only a radius is provided, calculate a random destination near edge of radius
-    var destination = 'destination' in params ? params.destination : await calculateDestination(origin, parseInt(params.radius));
+    if ('destination' in params) {
+      var destination = params.destination;
+    }
+    else if ('radius' in params) {
+      destination = await calculateDestination(origin, parseInt(params.radius));
+      num_waypoints--;
+    }
     var initial_route = await client
     .directions({
       params: {
@@ -47,16 +54,27 @@ async function calculateTrip(params) {
       },
       timeout: 2000 // milliseconds
     });
-    var points = polyline.decode(initial_route.data.routes[0].overview_polyline.points);
-    var x = Math.floor(points.length/(num_waypoints+1));
+    if (initial_route.data.status != "OK") {
+      throw "Google maps error: " + initial_route.data.status;
+    }
+    var points = polyline.decode(initial_route.data.routes[0].overview_polyline.points); // array of lat/lng points
+    var x = Math.floor(points.length/(num_waypoints+1)); // points array offset
+    var trip_length = geometry.computeLength(points.map((point) => [point[1], point[0]]));
     var waypoints = [];
-    var waypoint_radius = 50000; // how far out from each point we should search for a place
+    var waypoint_radius = Math.min(Math.ceil((trip_length/(num_waypoints+1))/2), 50000); // how far out from each point we should search for a place (in meters), 805 = (1609 m)/2
+    // Find all waypoints for trip
     for (let i = 1; i <= num_waypoints; i++) {
       var type = types[(i-1)%types.length];
-      var waypoint = await findPlace(points[i*x], waypoint_radius, waypoints, type);
-      if (waypoint != null) {
-        waypoints.push(waypoint);
+      try {
+        var waypoint = await findPlace(points[i*x], waypoint_radius, waypoints, origin, destination, type);
+      } catch (e) {
+        if (e == "Google maps error: ZERO_RESULTS") { // try one more time, with different type
+          waypoint = await findPlace(points[i*x], waypoint_radius, waypoints, origin, destination, "tourist_attraction");
+        } else {
+          throw e;
+        }
       }
+      waypoints.push(waypoint);
     }
     return {
         'origin': origin,
@@ -66,7 +84,7 @@ async function calculateTrip(params) {
   }
   
 
-  async function findPlace(location, radius, waypoints, type) {
+  async function findPlace(location, radius, waypoints, origin, destination, type) {
     var places = await client
       .placesNearby({
         params: {
@@ -77,21 +95,14 @@ async function calculateTrip(params) {
         },
         timeout: 1000
       });
-      if (places.data.status == "ZERO_RESULTS") {
-        places = await client
-        .placesNearby({
-          params: {
-            location: location,
-            radius: radius,
-            type: "tourist_attraction", 
-            key: process.env.GOOGLE_MAPS_API_KEY
-          },
-          timeout: 1000
-        });
+      if (places.data.status != "OK") {
+        throw "Google maps error: " + places.data.status;
       }
       var i = 0;
       // prevent duplicates waypoints
-      while (waypoints.includes(places.data.results[i])) {
+      while (waypoints.some(w => w.name == places.data.results[i].name) || 
+      places.data.results[i].name == origin ||
+      places.data.results[i].name == destination) {
         i++;
       }
       return places.data.results[i];
@@ -109,15 +120,25 @@ async function calculateTrip(params) {
         },
         timeout: 1000
       });
+    if (origin_details.data.status != "OK") {
+      throw "Google maps error: " + origin_details.data.status;
+    }
     var origin_coords = origin_details.data.candidates[0].geometry.location;
-    var dest_found = false;
-    while (!dest_found) {
-      var heading = Math.floor(Math.random() * 360); // Calculate random heading from origin
-      var dest_coords = geometry.computeOffset(origin_coords, radius*1609, heading); // Compute random destination coordinates
-      var dest = await findPlace([dest_coords.lat(), dest_coords.lng()], 30000, [], "tourist_attraction"); // Get name of nearby destination
-      if ('name' in dest) {
-        dest_found = true;
+    // Keep computing a different heading until we find a valid destination
+    while (true) {
+      try {
+        var heading = Math.floor(Math.random() * 360); // Calculate random heading from origin
+        var dest_coords = geometry.computeOffset(origin_coords, radius*1609, heading); // Compute random destination coordinates
+        var dest = await findPlace([dest_coords.lat(), dest_coords.lng()], 30000, [], origin, "", "tourist_attraction"); // Get name of nearby destination
+      } catch (e) {
+        if (e ==  "Google maps error: ZERO_RESULTS") {
+          console.log("Destination not found, retrying...");
+          continue;
+        } else {
+          throw e;
+        }
       }
+      break;
     }
     return dest.name;
   }
